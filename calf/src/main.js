e42e5c1aa5994e531498eeb38d4b5fd9be824541
zhuliang <liang.zhu02@enmotech.com>
2021-11-02 17:40:40 +0800

    冲突代码删除

--- calf/src/main.js
+++ calf/src/main.js
@@ -2,15 +2,10 @@ import Vue from 'vue'
 import router from './router'
 import './element'
 import App from './App.vue'
-import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-<<<<<<< HEAD
   render: (h) => h(App)
-=======
-  render: h => h(App),
->>>>>>> 587a878fcbefdf42c00286117f09bf273a92e7d8
 }).$mount('#app')
--- calf/src/pages/Login.vue
+++ calf/src/pages/Login.vue
@@ -5,14 +5,8 @@
 </template>
 
 <script>
-<<<<<<< HEAD
   export default {
     name: 'login'
   }
-=======
-export default {
-  name: 'login'
-}
->>>>>>> 587a878fcbefdf42c00286117f09bf273a92e7d8
 </script>
 

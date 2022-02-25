20c7f954514418e5c22628fec0a3c59ab1f38ee4
zhuliang <liang.zhu02@enmotech.com>
2022-02-25 10:17:28 +0800

    跳当前路由报错处理

--- calf/src/router/index.js
+++ calf/src/router/index.js
@@ -9,4 +9,11 @@ const router = new VueRouter({
   routes
 })
 
+// 跳当前路由报错处理
+const originalPush = VueRouter.prototype.push
+
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
+
 export default router
